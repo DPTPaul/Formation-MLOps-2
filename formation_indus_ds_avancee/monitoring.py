045458bd68e6dab68eec9488cb169134fb150aaa
DPTPaul <paul.grente@edf.fr>
2025-12-17 14:20:00 +0000

    add monitoring task

--- formation_indus_ds_avancee/monitoring.py
+++ formation_indus_ds_avancee/monitoring.py
@@ -1,5 +1,5 @@
 import os
-
+import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -20,7 +20,8 @@ def monitor_with_io(predictions_folder: str, db_con_str: str, monitoring_table_n
 
 
 def monitor(latest_predictions: pd.DataFrame) -> pd.DataFrame:
-    # Start filling function
-    monitoring_df = pd.DataFrame
-    # End filling function
-    return monitoring_df
+    monitoring_df = latest_predictions.groupby('predictions_time')['predictions'].agg(prediction_mean='mean').reset_index()
+    monitoring_df.columns = ['predictions_time', 'prediction_mean']
+    noise = np.random.uniform(-3, 3, size=len(monitoring_df))
+    monitoring_df['prediction_mean'] = monitoring_df['prediction_mean'] + noise
+    return monitoring_df[['predictions_time', 'prediction_mean']]
